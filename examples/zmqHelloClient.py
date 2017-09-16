c948bf088d7525394026a619e90abcd3c1791433
perhansson <per.hansson80@gmail.com>
2017-09-16 15:10:44 -0700

    add trailing changes

--- examples/zmqHelloClient.py
+++ examples/zmqHelloClient.py
@@ -14,10 +14,14 @@ socket.connect("tcp://localhost:5555")
 
 for req in range(10):
     print("Sending req %s " % req)
-    socket.send_string("Hello")
+    # bytes
     #socket.send(b"Hello")
-    
+    # unicode
+    socket.send_string("Hello")
+
+    # bytes
     #server_reply = socket.recv()
+    # unicode
     server_reply = socket.recv_string()
     print("Received reply from server for req %d: %s" % (req, server_reply))
 
